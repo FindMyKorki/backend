41ebc346f3e7d8ee022100f5c54cedcc08a32443
Szymon Mlonek <szymonmlnk@gmail.com>
2025-04-09 17:10:58 +0200

    fix: adjust auth to cooperate with swagger ui

--- app/users/auth.py
+++ app/users/auth.py
@@ -1,32 +1,41 @@
+from os import access
+
 from core.db_connection import supabase
 from profiles.utils import get_profile_data
 from profiles.dataclasses import Profile
 from fastapi import HTTPException, Depends
 from typing import Annotated
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, HTTPBearer
 from gotrue.types import UserResponse
 
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_scheme = HTTPBearer()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
-async def authenticate_user(access_token: Annotated[str, Depends(oauth2_scheme)]) -> UserResponse:
-    return await _authenticate_user(access_token)
+async def authenticate_user(auth_header: Annotated[str, Depends(oauth2_scheme)]) -> UserResponse:
+    # return access_token
+    return await _authenticate_user(auth_header.credentials)
 
-async def authenticate_profile(access_token: Annotated[str, Depends(oauth2_scheme)]) -> Profile:
+
+async def authenticate_profile(auth_header: Annotated[str, Depends(oauth2_scheme)]) -> Profile:
     try:
+        access_token = auth_header.credentials
         user_data = await _authenticate_user(access_token)
         profile = await get_profile_data(user_data.user.id)
-        
+
         if not profile:
             raise Exception
-        
+
         return profile
     except:
         raise HTTPException(status_code=401, detail='User unauthorized')
 
-async def authenticate_sign_out(access_token: Annotated[str, Depends(oauth2_scheme)]) -> str:
+
+async def authenticate_sign_out(auth_header: Annotated[str, Depends(oauth2_scheme)]) -> str:
+    access_token = auth_header.credentials
     await _authenticate_user(access_token)
     return access_token
+
 
 async def _authenticate_user(access_token: str) -> UserResponse:
     try:
