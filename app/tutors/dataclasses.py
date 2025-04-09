86f7c1863ee27b1a7df19b65e6d69c9876bf6c63
Szymon Mlonek <szymonmlnk@gmail.com>
2025-04-09 10:37:47 +0200

    fix: make tutor_profiles fields optional and include featured_review data in get request

--- app/tutors/dataclasses.py
+++ app/tutors/dataclasses.py
@@ -3,8 +3,8 @@ from typing import Optional
 
 
 class CreateTutorProfileRequest(BaseModel):
-    bio: str
-    bio_long: str
+    bio: Optional[str] = None
+    bio_long: Optional[str] = None
     contact_email: Optional[EmailStr] = None
     phone_number: Optional[str] = None
 
@@ -14,6 +14,8 @@ class CreateTutorProfileRequest(BaseModel):
 
 class TutorResponse(CreateTutorProfileRequest):
     rating: float
-    featured_review_id: Optional[str] = None  # featured_review id
+    featured_review_id: Optional[int] = None  # featured_review id
+    featured_review_rating: Optional[float] = None  # featured_review id
+    featured_review_comment: Optional[str] = None  # featured_review id
     full_name: Optional[str] = None
     avatar_url: Optional[str] = None
--- app/tutors/utils.py
+++ app/tutors/utils.py
@@ -8,7 +8,7 @@ async def get_tutor_profile_data(tutor_id: str) -> TutorResponse:
     tutor = (
         supabase.table('tutor_profiles')
         .select(
-            "id, bio, bio_long, rating, contact_email, phone_number, featured_review_id, profiles(full_name, avatar_url)")
+            "id, bio, bio_long, rating, contact_email, phone_number, profiles(full_name, avatar_url), reviews!tutor_profiles_featured_review_id_fkey(id, student_id, tutor_id, rating, comment, created_at)")
         .eq("id", tutor_id)
         .execute()
     )
@@ -26,7 +26,9 @@ def flatten_tutor_data(tutor_data: {}) -> TutorResponse:
         "rating": tutor_data["rating"],
         "contact_email": tutor_data["contact_email"],
         "phone_number": tutor_data["phone_number"],
-        "featured_review_id": tutor_data["featured_review_id"],
+        "featured_review_id": tutor_data["reviews"].get("id") if tutor_data.get("reviews") else None,
+        "featured_review_rating": tutor_data["reviews"].get("rating") if tutor_data.get("reviews") else None,
+        "featured_review_comment": tutor_data["reviews"].get("comment") if tutor_data.get("reviews") else None,
         "full_name": tutor_data["profiles"].get("full_name") if tutor_data.get("profiles") else None,
         "avatar_url": tutor_data["profiles"].get("avatar_url") if tutor_data.get("profiles") else None,
     }
